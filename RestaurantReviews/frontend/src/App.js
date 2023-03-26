ad48b7e5b4d9de09168cc4684fec77cf8e60b9f4
Miles Hollifield <mileshollifieldgfp@gmail.com>
2023-03-26 18:10:26 -0400

    [Implementation][Fixes] Implemented App.js and components; hotfixes
    Added route component files based on restaurant, their lists, adding reviews, and login. Installed prior version of react-router-dom to support Switch functionality.

--- RestaurantReviews/frontend/src/App.js
+++ RestaurantReviews/frontend/src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+// Importing React
+import React from "react";
+// Importing Switch, Route, Link
+import { Switch, Route, Link } from "react-router-dom";
+// Importing Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Importing AddReview
+import AddReview from "./components/add-review";
+// Importing Restaurant
+import Restaurant from "./components/restaurants";
+// Importing RestaurantsList
+import RestaurantsList from "./components/restaurants-list";
+// Importing Login
+import Login from "./components/login";
 
 function App() {
+  // Initializing a State variable
+  const [user, setUser] = React.useState(null);
+
+  // Login function
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  // Logout function
+  async function logout() {
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      {/* Nav Bar */}
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        {/* Restaurant Reviews Brand link */}
+        <a href="/restaurants" className="navbar-brand">
+          Restaurant Reviews
         </a>
-      </header>
+        <div className="navbar-nav mr-auto">
+          {/* Restaurants link */}
+          <li className="nav-item">
+            <Link to={"/restaurants"} className="nav-link">
+              Restaurants
+            </Link>
+          </li>
+          {/* Login/Logout link */}
+          <li className="nav-item">
+            {/* If user is logged in, show logout user */}
+            { user ? (
+              <a onClick={logout} className="nav-link" style={{cursor: 'pointer'}}>
+                Logout {user.name}
+              </a>
+              // Else, show login 
+            ) : (
+              <Link to={"/login"} className="nav-link">
+                Login
+              </Link>
+            )}
+          </li>
+        </div>
+      </nav>
+
+      <div className="container mt-3">
+        {/* Switch between routes */}
+        <Switch>
+          {/* Loads RestaurantsList */}
+          <Route exact path={["/", "/restaurants"]} component={RestaurantsList} />
+          {/* Loads AddReview component for user */}
+          <Route 
+            path="/restaurants/:id/review" 
+            render={(props) => (
+              <AddReview {...props} user={user} />
+            )}
+          />
+          {/* Loads Restaurants component for user */}
+          <Route
+            path="/restaurants/:id"
+            render={(props) => (
+              <Restaurant {...props} user={user} />
+            )}
+          />
+          {/* Loads Login component for user */}
+          <Route
+            path="/login"
+            render={(props) => (
+              <Login {...props} login={login} />
+            )}
+          />
+
+        </Switch>
+      </div>
     </div>
   );
 }
