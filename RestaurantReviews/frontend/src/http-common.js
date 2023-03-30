4707d10e46d92d2e31beb8145f44cb6adcd3b95c
Miles Hollifield <mileshollifieldgfp@gmail.com>
2023-03-30 01:49:06 -0400

    [Testing] Fixed regressive bug of connecting backend and frontend

--- RestaurantReviews/frontend/src/http-common.js
+++ RestaurantReviews/frontend/src/http-common.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export default axios.create({
     // Base URL for backend server
-    baseURL: "http://localhost:5000/api/v1/restaurants",
+    baseURL: "http://localhost:5000/api/v1/",
     // Set headers
     headers: {
         "Content-type": "application/json"
--- RestaurantReviews/frontend/src/services/restaurant.js
+++ RestaurantReviews/frontend/src/services/restaurant.js
@@ -9,7 +9,7 @@ class RestaurantDataService {
 
     // get function takes in id as parameter
     get(id) {
-        return http.get(`/restaurant?id=${id}`);
+        return http.get(`restaurant?id=${id}`);
     }
 
     // find function based on query, name, and page
@@ -19,22 +19,22 @@ class RestaurantDataService {
 
     // createReview function to create a new review
     createReview(data) {
-        return http.post("/review-new", data);
+        return http.post("restaurants/review-new", data);
     }
 
     // updateReview function to update a review
     updateReview(data) {
-        return http.put("/review-edit", data);
+        return http.put("restaurants/review-edit", data);
     }
 
     // deleteReview function to delete a review
     deleteReview(id, userId) {
-        return http.delete(`/review-delete?id=${id}`, {data:{user_id: userId}});
+        return http.delete(`restaurants/review-delete?id=${id}`, {data:{user_id: userId}});
     }
 
     // getCuisines function to return cuisines
     getCuisines(id) {
-        return http.get(`/cuisines`);
+        return http.get(`restaurants/cuisines`);
     }
 }
 
